d7e045726467c25153d311b840fb763bb7a746e0
ankesh2623 <ankeshkumarchaubey@gmail.com>
2024-04-04 14:58:51 +0530

    final project

--- server.cpp
+++ server.cpp
@@ -1,51 +1,206 @@
-// server.cpp
-
-#include <iostream>
-#include <unistd.h>
+#include <bits/stdc++.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h> // for TCP_KEEPIDLE, TCP_KEEPINTVL, TCP_KEEPCNT
+#include <cstdlib> // for exit
+#include <errno.h>
+#include <string.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <thread>
+#include <mutex>
+#define MAX_LEN 200
+#define NUM_COLORS 6
 
-int main() {
-    // Create socket
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        std::cerr << "Error creating socket\n";
-        return 1;
-    }
+using namespace std;
 
-    // Bind socket to port
-    sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(12345); // Port number
-    server_address.sin_addr.s_addr = INADDR_ANY;
+struct terminal
+{
+	int id;
+	string name;
+	int socket;
+	thread th;
+};
 
-    if (bind(server_socket, reinterpret_cast<sockaddr*>(&server_address), sizeof(server_address)) == -1) {
-        std::cerr << "Error binding socket to port\n";
-        return 1;
-    }
+vector<terminal> clients;
+string def_col="\033[0m";
+string colors[]={"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m","\033[36m"};
+int seed=0;
+mutex cout_mtx,clients_mtx;
 
-    // Listen for connections
-    if (listen(server_socket, 5) == -1) {
-        std::cerr << "Error listening for connections\n";
-        return 1;
-    }
+string color(int code);
+void set_name(int id, char name[]);
+void shared_print(string str, bool endLine);
+int broadcast_message(string message, int sender_id);
+int broadcast_message(int num, int sender_id);
+void end_connection(int id);
+void handle_client(int client_socket, int id);
 
-    std::cout << "Server is listening for connections...\n";
+int main()
+{
+	int server_socket;
+	if((server_socket=socket(AF_INET,SOCK_STREAM,0))==-1)
+	{
+		perror("socket: ");
+		exit(-1);
+	}
 
-    // Accept incoming connections
-    sockaddr_in client_address;
-    socklen_t client_address_size = sizeof(client_address);
-    int client_socket = accept(server_socket, reinterpret_cast<sockaddr*>(&client_address), &client_address_size);
-    if (client_socket == -1) {
-        std::cerr << "Error accepting connection\n";
-        return 1;
-    }
+	struct sockaddr_in server;
+	server.sin_family=AF_INET;
+	server.sin_port=htons(10000);
+	server.sin_addr.s_addr=INADDR_ANY;
+	bzero(&server.sin_zero,0);
 
-    std::cout << "Connection accepted from " << inet_ntoa(client_address.sin_addr) << ":" << ntohs(client_address.sin_port) << std::endl;
+	if((bind(server_socket,(struct sockaddr *)&server,sizeof(struct sockaddr_in)))==-1)
+	{
+		perror("bind error: ");
+		exit(-1);
+	}
 
-    // Close server socket
-    close(server_socket);
+	if((listen(server_socket,8))==-1)
+	{
+		perror("listen error: ");
+		exit(-1);
+	}
 
-    return 0;
+	struct sockaddr_in client;
+	int client_socket;
+	unsigned int len=sizeof(sockaddr_in);
+
+	cout<<colors[NUM_COLORS-1]<<"\n\t  ====== Welcome to the chat-room ======   "<<endl<<def_col;
+
+	while(1)
+	{
+		if((client_socket=accept(server_socket,(struct sockaddr *)&client,&len))==-1)
+		{
+			perror("accept error: ");
+			exit(-1);
+		}
+		seed++;
+		thread t(handle_client,client_socket,seed);
+		lock_guard<mutex> guard(clients_mtx);
+		clients.push_back({seed, string("Anonymous"),client_socket,(move(t))});
+	}
+
+	for(int i=0; i<clients.size(); i++)
+	{
+		if(clients[i].th.joinable())
+			clients[i].th.join();
+	}
+
+	close(server_socket);
+	return 0;
+}
+
+string color(int code)
+{
+	return colors[code%NUM_COLORS];
+}
+
+// Set name of client
+void set_name(int id, char name[])
+{
+	for(int i=0; i<clients.size(); i++)
+	{
+			if(clients[i].id==id)	
+			{
+				clients[i].name=string(name);
+			}
+	}	
+}
+
+// For synchronisation of cout statements
+void shared_print(string str, bool endLine=true)
+{	
+	lock_guard<mutex> guard(cout_mtx);
+	cout<<str;
+	if(endLine)
+			cout<<endl;
+}
+
+// Broadcast message to all clients except the sender
+int broadcast_message(string message, int sender_id)
+{
+	char temp[MAX_LEN];
+	strcpy(temp,message.c_str());
+	for(int i=0; i<clients.size(); i++)
+	{
+		if(clients[i].id!=sender_id)
+		{
+			send(clients[i].socket,temp,sizeof(temp),0);
+		}
+	}		
+}
+
+// Broadcast a number to all clients except the sender
+int broadcast_message(int num, int sender_id)
+{
+	for(int i=0; i<clients.size(); i++)
+	{
+		if(clients[i].id!=sender_id)
+		{
+			send(clients[i].socket,&num,sizeof(num),0);
+		}
+	}		
+}
+
+void end_connection(int id)
+{
+	for(int i=0; i<clients.size(); i++)
+	{
+		if(clients[i].id==id)	
+		{
+			lock_guard<mutex> guard(clients_mtx);
+			close(clients[i].socket);
+			clients[i].th.detach();
+			clients.erase(clients.begin()+i);
+			break;
+		}
+	}				
+}
+
+void handle_client(int client_socket, int id)
+{
+	char name[MAX_LEN],str[MAX_LEN];
+	recv(client_socket,name,sizeof(name),0);
+	set_name(id,name);	
+
+	// Display welcome message
+	string welcome_message=string(name)+string(" has joined");
+	broadcast_message("#NULL",id);	
+	broadcast_message(id,id);								
+	broadcast_message(welcome_message,id);	
+	shared_print(color(id)+welcome_message+def_col);
+	
+	while(1)
+	{
+		int bytes_received=recv(client_socket,str,sizeof(str),0);
+		if (bytes_received <= 0)
+        {
+            // Client disconnected
+            string message = string(name) + string(" has left");
+            broadcast_message("#NULL", id);
+            broadcast_message(id, id);
+            broadcast_message(message, id);
+            shared_print(color(id) + message + def_col);
+            end_connection(id);
+            return;
+        }
+		if(strcmp(str,"#exit")==0)
+		{
+			// Display leaving message
+			string message=string(name)+string(" has left");		
+			broadcast_message("#NULL",id);			
+			broadcast_message(id,id);						
+			broadcast_message(message,id);
+			shared_print(color(id)+message+def_col);
+			end_connection(id);							
+			return;
+		}
+		broadcast_message(string(name),id);					
+		broadcast_message(id,id);		
+		broadcast_message(string(str),id);
+		shared_print(color(id)+name+" : "+def_col+str);		
+	}	
 }
